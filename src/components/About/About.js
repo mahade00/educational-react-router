39ff465d690b6bf7ea332bcddc87219e229439db
Mahade Hassan <mahade.hassan.bd@gmail.com>
2023-03-11 08:28:36 +0600

    about us added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import AboutImg from '../images/about_us.avif'
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
+        <div className='about-main'>
+            <div className='about'>
+                <h1 className='font-semibold text-5xl text-indigo-900'>About Us</h1>
+                <p className='text-indigo-900 text-base pt-5'>EdTech is fast growing Education and Software Service platform that keeps culture and creativity at the heart of everything we do. Our mission is to help customers unlock to creativity and build exceptional content using uniqely powerful design platform and our tirelessly helpful support and education resource.</p>
+            </div>
+            <div className='about-image'>
+                <img src={AboutImg} alt="About Us" />
+            </div>
         </div>
     );
 };
