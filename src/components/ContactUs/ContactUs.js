3eb1571fe1cd993f193e7563a92bafed49854ec3
Mahade Hassan <mahade.hassan.bd@gmail.com>
2023-03-11 07:54:43 +0600

    contact added

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './ContactUs.css';
 
 const ContactUs = () => {
     return (
-        <div>
-            <h2>This is Contact Us</h2>
+        <div className='contact-main'>
+            <div className='contact-name'>
+                <div >
+                    <p className='font-medium text-indigo-900'>First Name:</p>
+                    <input className='input-text' />
+                </div>
+                <div>
+                    <p className='font-medium text-indigo-900'>Last Name:</p>  
+                    <input className='input-text' /> 
+                </div>                  
+            </div>
+            <div className='contact-sub font-medium text-indigo-900 pt-5 '>
+                <p className='pl-14'>Contact Number:</p>
+                <input className='input-sub' />
+            </div>
+            <div className='contact-sub font-medium text-indigo-900 pt-5'>
+                <p>Subject:</p>
+                <input className='input-sub'/>
+            </div>
+            <div className='contact-messge font-medium text-indigo-900 pt-5'>
+                <p>Message:</p>
+                <input className='input-message'/>
+            </div>
+            <div>
+                <button className='contact-btn'>Submit</button>
+            </div>
         </div>
     );
 };
